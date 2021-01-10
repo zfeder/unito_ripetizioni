46dcfed6c8bf73b7a03fa85929b037b2f44e843c
Giorgio <giorgio.licitra264@edu.unito.it>
2021-01-10 16:34:07 +0100

    second commit

---
+++ unito_ripetizioni/src/main/java/DAO/DAO.java
@@ -0,0 +1,18 @@
+package DAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "DAO")
+public class DAO extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, java.io.IOException {
+
+    }
+}
