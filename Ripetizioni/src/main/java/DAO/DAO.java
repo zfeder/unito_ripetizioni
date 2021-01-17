4edb407075ebcf535b0aef704c4b14271aa59d2f
Giorgio <giorgio.licitra264@edu.unito.it>
2021-01-17 02:14:33 +0100

    login senza sessione

--- Ripetizioni/src/main/java/DAO/DAO.java
+++ Ripetizioni/src/main/java/DAO/DAO.java
@@ -51,14 +51,16 @@ public class DAO {
     }
 
 
-    //CONTROL UTENTE DB
+    //CONTROL
     public static boolean checkDB(String futente, String fpassword) {
         Connection conn1 = null;
         boolean t = false;
+        //System.out.println("Utente: " + futente);
+        //System.out.println("Password: " + fpassword);
         try {
             conn1 = DriverManager.getConnection(url1, user, password);
             if (conn1 != null) {
-                //System.out.println("Connected to the database test");
+                System.out.println("Connected to the database test");
             }
             Statement st = conn1.createStatement();
             ResultSet rs = st.executeQuery("SELECT * FROM UTENTE");
@@ -67,6 +69,10 @@ public class DAO {
                 String utente = rs.getString("ID_UTENTE");
                 String password = rs.getString("PASSWORD");
                 String ruolo = rs.getString("RUOLO");
+                if(ruolo.equals(s))
+                    System.out.println("Questo utente è un amministratore");
+                else
+                    System.out.println("Questo utente non è un amministratore");
                 if (utente.equals(futente) && password.equals(fpassword)) {
                     t = true;
                 }
@@ -85,14 +91,13 @@ public class DAO {
         return t;
     }
 
-    //CONTROL ADMIN DB
     public static boolean checkAdmin(String futente) {
         Connection conn1 = null;
         boolean b = false;
         try {
             conn1 = DriverManager.getConnection(url1, user, password);
             if (conn1 != null) {
-                //System.out.println("Connected to the database test");
+                System.out.println("Connected to the database test");
             }
             Statement st = conn1.createStatement();
             ResultSet rs = st.executeQuery("SELECT * FROM UTENTE");
@@ -101,11 +106,11 @@ public class DAO {
                 String utente = rs.getString("ID_UTENTE");
                 String ruolo = rs.getString("RUOLO");
                 if(utente.equals(futente) && ruolo.equals(s)) {
-                    System.out.println("Ruolo: Amministratore");
+                    System.out.println("Amministratore");
                     b = true;
                 }
                 else
-                    System.out.println("Ruolo: Non Amministratore");
+                    System.out.println("Non Amministratore");
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
@@ -121,5 +126,7 @@ public class DAO {
         return b;
     }
 
+    }
 
-}
+
+
--- Ripetizioni/src/main/java/pack/Servlet02.java
+++ Ripetizioni/src/main/java/pack/Servlet02.java
@@ -33,53 +33,17 @@ public class Servlet02 extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        try {
-            String utente = request.getParameter("utente");
-            String password = request.getParameter("password");
-            System.out.println("Utente: " + utente);
-            System.out.println("Password: " + password);
-            String url = response.encodeURL("index.html");
-            HttpSession s = request.getSession();
-            String azione = request.getParameter("no");
-            if (azione != null) {
-                s.invalidate();
-                out.println("<p>Sessione invalidata!</p>");
+        String utente = request.getParameter("utente");
+        String password = request.getParameter("password");
+        String stato = "Nonloggato";
+        if (DAO.checkDB(utente, password)) {
+            if (DAO.checkAdmin(utente)) {
+                stato = "Admin";
             } else {
-                out.print("<p>Stato della sessione: ");
-                if (s.isNew())
-                    out.println(" nuova sessione</p>");
-                else out.println(" vecchia sessione</p>");
-                out.println("<p>ID di sessione: " + s.getId() + "</p>");
-                out.println("<p>Data di creazione: " + new Date(s.getCreationTime()) + "</p>");
-                out.println("<p>Max inactive time interval (in secondi): "
-                        + s.getMaxInactiveInterval() + "</p>");
-                if (DAO.checkDB(utente, password)) {
-                    DAO.checkAdmin(utente);
-
-                    out.println("Benvenuto, " + utente);
-                    {
-                        if (utente != null)
-                            s.setAttribute("utente", utente);
-                        try {
-                            out.println("<html>");
-                            out.println("<head>");
-                            out.println("<title>Servlet02</title>");
-                            out.println("</head>");
-                            out.println("<body>");
-                            out.println("</body>");
-                            out.println("</html>");
-                        } finally {
-                            out.close();
-                        }
-                    }
-                } else
-                    out.println("Utente Errato!");
+                stato = "Utente";
             }
         }
-        finally{
-            if (out != null)
-                out.close();
-        }
+        out.println(stato);
     }
 }
 
