abf09eae1e8f04bf82b8059a61fc61ec85d8e9e4
Giorgio <giorgio.licitra264@edu.unito.it>
2021-01-28 17:09:13 +0100

    Aggiunto corso

---
+++ Ripetizioni/src/main/java/DAO/Corso.java
@@ -0,0 +1,27 @@
+package DAO;
+
+public class Corso {
+    private String idInsegnamento;
+    private String idDocenteC;
+    private String idCorso;
+
+
+    public Corso(String idInsegnamento, String idCorso , String idDocente) {
+        this.idInsegnamento = idInsegnamento;
+        this.idDocenteC = idDocenteC;
+        this.idCorso = idCorso;
+    }
+
+
+    public String getidInsegnamento() {
+        return idInsegnamento;
+    }
+
+    public String getidDocenteC() {
+        return idDocenteC;
+    }
+
+    public String getidCorso() {
+        return idCorso; }
+
+}
--- Ripetizioni/src/main/java/pack/ServletAddCorso.java
+++ Ripetizioni/src/main/java/pack/ServletAddCorso.java
@@ -7,8 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "ServletAddMat")
-public class ServletAddMat extends HttpServlet {
+@WebServlet(name = "ServletAddCorso")
+public class ServletAddCorso extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
