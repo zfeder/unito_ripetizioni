8f802e544ea0b434d3ca320d5450d6ba557f55be
Giorgio <giorgio.licitra264@edu.unito.it>
2021-03-15 17:57:41 +0100

    merge remove servlet

---
+++ Ripetizioni/src/main/java/pack/ServletRemove.java
@@ -0,0 +1,104 @@
+package pack;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.*;
+import java.sql.SQLException;
+import java.util.Date;
+
+import DAO.*;
+
+@WebServlet(name = "ServletRemove", urlPatterns = {"/ServletRemove"})
+public class ServletRemove extends HttpServlet {
+    public void init(ServletConfig conf) throws ServletException {
+        DAO.registerDriver();
+    }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        String azione = request.getParameter("azione");
+        System.out.println(azione);
+        switch (azione) {
+            case "removeCorso":
+                response.setContentType("text/html;charset=UTF-8");
+                PrintWriter out = response.getWriter();
+                String idCorRem = request.getParameter("idCorRem");
+                System.out.println(idCorRem);
+                String stato = "Errore nel rimuovere il corso";
+
+                if (idCorRem != null) {
+                    CorsoREM p = new CorsoREM(idCorRem);
+                    System.out.println("PARAMETRI INSERITI CORRETTAMENTE");
+                    System.out.println(p.getCorsoREM());
+                    DAO.removeCorso(p);
+                    stato = "Corso rimosso correttamente";
+                }
+                out.print(stato);
+                break;
+            case "removeDocente":
+                response.setContentType("text/html;charset=UTF-8");
+                PrintWriter out1 = response.getWriter();
+                String idDocRem = request.getParameter("idDocRem");
+                System.out.println(idDocRem);
+                String stato1 = "Errore nel rimuovere docente";
+
+                if (idDocRem != null) {
+                    DocenteREM p = new DocenteREM(idDocRem);
+                    System.out.println("PARAMETRI INSERITI CORRETTAMENTE");
+                    System.out.println(p.getidDocenteREM() + "idocente");
+                    DAO.removeDocente(p);
+                    stato1 = "Docente rimosso correttamente";
+                }
+                out1.print(stato1);
+                break;
+
+            case "removeMateria":
+                response.setContentType("text/html;charset=UTF-8");
+                PrintWriter out2 = response.getWriter();
+                String idMatRem = request.getParameter("idMatRem");
+                System.out.println(idMatRem);
+                String stato2 = "Errore nel rimuovere il corso";
+
+                if (idMatRem != null) {
+                    MateriaREM p = new MateriaREM(idMatRem);
+                    System.out.println("PARAMETRI INSERITI CORRETTAMENTE");
+                    System.out.println(p.getMateriaREM());
+                    DAO.removeMateria(p);
+                    stato2 = "Materia rimossa correttamente";
+                }
+                out2.print(stato2);
+
+                break;
+
+
+        }
+
+
+    }
+}
+
+
