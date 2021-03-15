db54b1369e1019d85ab5fe1cd380c6e8856ad328
Giorgio <giorgio.licitra264@edu.unito.it>
2021-03-15 17:27:44 +0100

    fusion login and logout

---
+++ Ripetizioni/src/main/java/pack/ServletLogin.java
@@ -0,0 +1,73 @@
+package pack;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.*;
+import java.util.Date;
+
+import DAO.*;
+
+@WebServlet(name = "ServletLogin", urlPatterns = {"/ServletLogin"})
+public class ServletLogin extends HttpServlet {
+    public void init(ServletConfig conf) throws ServletException {
+        DAO.registerDriver();
+    }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String azione = request.getParameter("azione");
+        System.out.println(azione);
+        switch (azione) {
+            case "login":
+                response.setContentType("text/html;charset=UTF-8");
+                PrintWriter out = response.getWriter();
+                System.out.println("Collegato alla servlet02");
+                String utente = request.getParameter("utente");
+                String password = request.getParameter("password");
+                String stato = "Nonloggato";
+                if (DAO.checkDB(utente, password)) {
+                    HttpSession s = request.getSession();
+                    s.setAttribute("utente", utente);
+                    s.setAttribute("password", password);
+                    if (DAO.checkAdmin(utente)) {
+                        stato = "Admin";
+                    } else {
+                        stato = "Utente";
+                    }
+                }
+                System.out.println(stato);
+
+                out.println(stato);
+                break;
+            case "logout":
+                response.setContentType("text/html;charset=UTF-8");
+                HttpSession s = request.getSession();
+                System.out.println("Logout effettuato con successo");
+                s.invalidate();
+                PrintWriter out1 = response.getWriter();
+                out1.println("Logout effettuato con successo");
+                break;
+
+        }
+
+
+    }
+}
+
+
